20c689ed403986804063a6c596d75d8613c640f9
sphariab@gmail.com <sphariab@gmail.com>
2020-05-22 22:13:31 +0300

    Updates

---
+++ precache-manifest.67d3074e066ed47162e9f3e63a65371d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47943b0bb16f675328cf5653485f3b1c",
+    "url": "/Crud-table/index.html"
+  },
+  {
+    "revision": "9493ce41434622460c32",
+    "url": "/Crud-table/static/js/2.8f6c4cef.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/Crud-table/static/js/2.8f6c4cef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "635cceca34a650ee1b0c",
+    "url": "/Crud-table/static/js/main.3e53b2ee.chunk.js"
+  },
+  {
+    "revision": "409d0fde6eae256cb822",
+    "url": "/Crud-table/static/js/runtime-main.dee458be.js"
+  }
+]);
